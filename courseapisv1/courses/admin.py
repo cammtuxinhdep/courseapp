015df41c27c843ab24e4cbc30f5f5c23919a149e
cammtuxinhdep <camstuscools@gmail.com>
2025-02-21 16:28:47 +0700

    firstdemo

---
+++ courseapisv1/courses/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from courses.models import Category, Courses, Lesson
+from django.utils.html import mark_safe
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+class LessonForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget)
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+class MyCourseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'subject', 'active', 'created_date', 'category']
+    search_fields = ['subject']
+    list_filter = ['id', 'created_date']
+    list_editable = ['subject']
+    readonly_fields = ['image_view']
+
+    def image_view(self, course):
+        return mark_safe(f"img src='/static/{course.image.name}' width='120' />")
+
+class MyLessonAdmin(admin.ModelAdmin):
+    form=LessonForm
+
+admin.site.register(Category)
+admin.site.register(Courses, MyCourseAdmin)
+admin.site.register(Lesson, MyLessonAdmin)
